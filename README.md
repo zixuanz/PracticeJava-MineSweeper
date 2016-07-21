2f9ce78ab1c7332bdbf085a8027a66b6f8b45c6c
zixuanz <Celleria@Zixuans-MacBook-Pro.local>
2016-07-21 00:14:55 -0700

    Modify ConsoleGame and Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+Project Name:
+=============
+MineSweep Game
+
+Project Goal:
+=============
+To practice programming in Java, including IO stream, exceptions' solution, and class design.
+
+Project Description:
+====================
+There will be two style of MineSweep Game in this project. One is playing in console. The other one will be with GUI.
+Currently, only game in console is done.
+
+
+To be continued...
--- src/ConsoleGame.java
+++ src/ConsoleGame.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ConsoleGame {
 	
@@ -5,6 +8,13 @@ public class ConsoleGame {
 	private int result;										//rest blocks
 	private boolean mine;									//record win or not
 	private int sweep[][];									//record which is sweep
+	private static boolean start = true;					//whether start a new game or not
+	
+	//directly start a game
+	ConsoleGame() throws IOException{
+		startGame();
+	}
+	
 	
 	ConsoleGame(short height, short width, short num){
 		resetGame(height, width, num);
@@ -23,7 +33,7 @@ public class ConsoleGame {
 	}
 	
 	
-	//user sweep a block and check the cases
+	//user sweeps a block and check the cases
 	public void sweepMine(int row, int col){
 		
 		if(map.getMap()[row][col] == 'O'){
@@ -35,7 +45,7 @@ public class ConsoleGame {
 			
 			zeroSweep(row, col);
 			
-			/*        bad idea. but good for think
+			/*        bad idea. but good for thinking
 			int left = col;
 			int right = col;
 			int top = row;
@@ -171,6 +181,158 @@ public class ConsoleGame {
 			}
 			System.out.println();
 		}
+	}
+	
+	public void startGame() throws IOException{
+		
+		short height = 0;
+		short width = 0;
+		short num = 0;
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		String input = "";
+		
+		System.out.println("======================================================================================================");
+		System.out.println(" How to play:");
+		System.out.println(" >To start game, please input the HEIGHT and WIDTH of map and the NUM of MINES in this map sequencely.");
+		System.out.println(" >The range for WIDTH and HEIGHT is 1 ~ 99");
+		System.out.println("  Eg. 4 3 2. It indicates that system will create a 4x3 map with 2 mines.");
+		System.out.println(" >After input, type Return on key board, the map will be printed on screen. "
+				+ "				\n X represents unsweeped block. * represents mine. And numbers represent how many mines in 3x3 area.");
+		System.out.println("  Eg. XX1\n     X*X\n     XXX");
+		System.out.println(" >Each time input the row and col number of the block that you want to sweep.");
+		System.out.println(" >When only the blocks with mine left in the map, WIN the game. /n Enjoy!/n/n");
+		System.out.println("======================================================================================================");
+		
+		while(start){
+			start = false;
+			input = "";
+			while(input.length() == 0 || !isValidMap(height, width, num)){
+				
+				System.out.println("Please input height and width of map and number of mines here:");
+				input = buffer.readLine();
+				
+				String temp[] = input.split(" ");
+				
+				if (temp.length > 3){
+					System.out.println("ERROR: Input too many values. Please input again!");
+					input = "";
+					continue;
+				}else if(temp.length < 3){
+					System.out.println("ERROR: Input less values. Please input again!");
+					input = "";
+					continue;
+				}
+				try{
+					height = Short.parseShort(temp[0]);
+					width = Short.parseShort(temp[1]);
+					num = Short.parseShort(temp[2]);
+				}catch(NumberFormatException e){
+					System.out.println("ERROR: Invalid Input. Input may not be numbers. Please input again!");
+					input = "";
+				}
+			}
+			
+			System.out.println("\n======================================================================================================\n");
+			
+			resetGame(height, width, num);
+			prettyPrint();
+			
+			while(!mine){
+				input = "";
+				short row = 0;
+				short col = 0;
+				while(input.length() == 0 || !isValidAction(row, col)){
+					
+					System.out.println("Please input the row and colomn of map that you want to sweep:");
+					input = buffer.readLine();
+					
+					String temp[] = input.split(" ");
+					
+					if (temp.length > 2){
+						System.out.println("ERROR: Input too many values. Please input again!");
+						input = "";
+						continue;
+					}else if(temp.length < 2){
+						System.out.println("ERROR: Input less values. Please input again!");
+						input = "";
+						continue;
+					}
+					row = Short.parseShort(temp[0]);
+					col = Short.parseShort(temp[1]);
+				}
+				
+				sweepMine(row, col);
+				prettyPrint();
+				System.out.println("\n======================================================================================================\n");
+				
+				if(isWin()){
+					System.out.println("\\\\\\\\Congratulations! You win!////");
+					break;
+				}
+			}
+			if(getMine()){
+				System.out.println("Sorry. You failed.");
+			}
+			
+			System.out.println("\n======================================================================================================\n");
+			
+			input = "";
+			while(input.length() == 0){
+				
+				System.out.println("Do you want to start a new game? (Y/N)");
+				input = buffer.readLine();
+				
+				String temp[] = input.split(" ");
+				
+				if (temp.length > 1){
+					System.out.println("ERROR: Input too many values. Please input again!");
+					input = "";
+					continue;
+				}
+				
+				if(input.equalsIgnoreCase("Y") || input.equalsIgnoreCase("yes")){
+					start = true;
+				}else if(input.equalsIgnoreCase("N") || input.equalsIgnoreCase("no")){
+					start = false;
+				}else{
+					System.out.println("ERROR: Invalid Input. End Game!");
+				}
+			}
+			
+		}
+	}
+	
+
+	public boolean isValidMap(short height, short width, short num){
+		boolean flag = true;
+		if(height<0 || height>99){
+			System.out.println("ERROR: The value of height is invalid. Please input height in 0 and 99!");
+			flag = false;
+		}
+		if(width<0 || width>99){
+			System.out.println("ERROR: The value of width is invalid. Please input height in 0 and 99!");
+			flag = false;
+		}
+		if((num > width * height | num <= 0) && flag){
+			System.out.println("ERROR: The value of number is invalid. Please input available Mine numbers!");
+			flag = false;
+		}
+		return flag;
+	}
+	
+
+	public boolean isValidAction(short row, short col){
+		boolean flag = true;
+		if(row<0 || row>map.getMapHeight()){
+			System.out.println("ERROR: The value of row is not valid. Please input row in 0 and " + map.getMapHeight() + "!");
+			flag = false;
+		}
+		if(col<0 || col>map.getMapWidth()){
+			System.out.println("ERROR: The value of width is not valid. Please input height in 0 and " + map.getMapWidth() + "99!");
+			flag = false;
+		}
+		
+		return flag;
 	}
 
 }
